7bd2e71f8dc923d1c8fe63621cd9b8dd7ce85f98
chimako3 <kiatama333@gmail.com>
2018-06-28 22:19:23 +0900

    File added
    index.php
    server.bat

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+//comemt
+#comemt
+/*
+commemt
+commemt
+*/
+
+echo "hello from the TOP!";
+echo "hello from the TOP again!";
+ ?>
+<!DOCTYPE html>
+<html lang="ja">
+<body>
+  <p>Hello World <?php echo "from PHP"; ?></p>
+</body>
+</html>
---
+++ server.bat
@@ -0,0 +1,4 @@
+:: server.bat
+pushd %~dp0
+php -S 192.168.100.100:8000
+popd
